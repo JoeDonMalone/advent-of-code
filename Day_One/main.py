62c5eb91392a40eb71665f87230df4b1f6ac1675
Joe Malone <joe.d.malone@gmail.com>
2022-02-26 21:01:26 -0600

    broke 'parts 1' and 'parts 2' into separate functions for clarity

--- Day_One/main.py
+++ Day_One/main.py
@@ -1,14 +1,21 @@
 with open("./Day_One/input.txt") as File:
    input = File.read().strip().split("\n")
- 
-prev_read = 0
-depth_increases = 0
+
+# Part 1
+def count_depth_increases(input):
+    prev_read = 0
+    depth_increases = 0
+        
+    for i,v in enumerate(input):
+        if int(v) > prev_read and i != 0:
+            print('Prev_read:', prev_read)
+            print('current_read:', v)
+            depth_increases += 1
+            print('current_score:', depth_increases)
+        prev_read = int(v)
+    print(depth_increases)
     
-for i,v in enumerate(input):
-    if int(v) > prev_read and i != 0:
-        print('Prev_read:', prev_read)
-        print('current_read:', v)
-        depth_increases += 1
-        print('current_score:', depth_increases)
-    prev_read = int(v)
-print(depth_increases)
+def window_avg_increase(input):
+    pass
+
+count_depth_increases(input)
