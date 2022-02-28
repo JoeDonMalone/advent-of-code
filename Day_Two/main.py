dc035fec0937834412e016d4bcd0b4a290255299
Joe Malone <joe.d.malone@gmail.com>
2022-02-27 20:18:41 -0600

    Part One Complete!

--- Day_Two/main.py
+++ Day_Two/main.py
@@ -1,8 +1,33 @@
-with open("./Day_Two/input.txt") as File:
-    input = File.read().strip().split("\n")
-
 # Part 1
+class Main():
 
-
+    def __init__(self, input):
+        self.input = input
+    
+    def partOne(input: str) -> int:
+        
+        map = {
+            "depth": 0,
+            "hPos": 0,
+            "up": lambda x: map["depth"] - x,
+            "down": lambda x: map["depth"] + x,
+            "forward": lambda x: map["hPos"] + x
+        }
+        for i, v in enumerate(input):
+            move, distance = v.split(" ")
+            if move == "up":
+                map["depth"] = map["up"](int(distance))
+            elif move == "down":
+                map["depth"] = map["down"](int(distance))
+            else:
+                map["hPos"] = map["forward"](int(distance))
+        
+        finalMove = map["depth"] * map["hPos"]
+        print(finalMove)
 # Part 2
 
+with open("./Day_Two/input.txt") as File:
+    input = File.read().strip().split("\n")
+    
+main = Main
+main.partOne(input)
