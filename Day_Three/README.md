1092469d9fe08fbd6d4a8ab7abc488813370c6a2
Joe Malone <joe.d.malone@gmail.com>
2022-02-27 22:43:53 -0600

    Initialized Day Three, test input for p1, and puzzle input.txt

---
+++ Day_Three/README.md
@@ -0,0 +1,15 @@
+# Day 3
+
+## Part 1:
+
+### Understandings:
+
+### Observations:
+
+## Part 2:
+
+### Understandings:
+
+### Observations:
+
+### Ambiguities:
---
+++ Day_Three/performance_testing.py
@@ -0,0 +1,16 @@
+from time import perf_counter
+from main import Main
+
+with open("./Day_Three/input.txt") as File:
+    input = File.read().strip().split("\n")
+main = Main
+if __name__ == "__main__":
+    start = perf_counter()
+    main.partOne(input)
+    duration = perf_counter() - start
+    print(f"Duration: {duration}")
+    
+    start = perf_counter()
+    main.partTwo(input)
+    duration = perf_counter() - start
+    print(f"Duration: {duration}")
