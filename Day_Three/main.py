c68870aba6a933414df4424b240a2d2d02d0ce65
Joe Malone <joe.d.malone@gmail.com>
2022-03-04 14:00:58 -0600

    Completed Day Three!

--- Day_Three/main.py
+++ Day_Three/main.py
@@ -1,4 +1,3 @@
-# Part 1
 with open("./Day_Three/input.txt") as File:
     input = File.read().strip().split("\n")
 
@@ -7,48 +6,25 @@ class Main():
     def __init__(self, input):
         self.input = input
     
-        
+    
     def partOne(input: str) -> int:
-        
-        # finalArr = [{"0":0, "1":0}] * len(input[0])
-        finalArr = [
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}, 
-                    {0: 0, 1: 0}
-                    ]
-        gam = [0] * len(finalArr)
-        eps = [0]* len(finalArr)
-        
+        finalArr = [ {0:0, 1:0} for _ in range(len(input[0]))]
+
         for row in input:
             for i, v in enumerate(row):
                 v = int(v)
-                # print('FinalArr vs bit:', finalArr[i][v])
                 finalArr[i][v] += 1
-        
         print(finalArr)
         
+        gam = [0] * len(finalArr)
+        eps = [0]* len(finalArr)
+        
         for j, v in enumerate(finalArr):
-            print(j, v[1])
-            if v[0] > v[1]:
-                gam[j] = "0"
-                eps[j] = "1"
-            else: 
-                gam[j] = "1"
-                eps[j] = "0"
-            
-        print("".join(gam))
-        print("".join(eps))
+            gam[j] = "0" if v[0]>v[1] else "1"
+            eps[j] = "1" if v[0]>v[1] else "0"
         
         def binToDec(bin):
+            print(bin)
             dec = 0
             for i in range(len(bin)):
                 digit = bin.pop()
@@ -56,14 +32,61 @@ class Main():
                     dec = dec + pow(2, i)
             return(dec)
         
-        powerConsumption = lambda x,y: binToDec(x) * binToDec(y)
+        powerConsumption = binToDec(gam) * binToDec(eps)
             
-        print(powerConsumption(gam, eps))
+        print(powerConsumption)
             
 # Part 2
     def partTwo(input: str) -> int:
-        pass
-
+        arr = input
+        
+        def binToDec(bin):
+            dec = 0
+            bin = [i for i in bin]
+            for i in range(len(bin)):
+                digit = bin.pop()
+                if digit == '1':
+                    dec = dec + pow(2, i)
+            return(dec)
+        
+        
+        searchVal = 0
+        rowIndex = 0
+        charIndex = 0
+        finalVal = []
+        
+        while rowIndex <= len(input[0])-1:
+            left = []
+            right = []
+            for i, row in enumerate(input):
+                left.append(row) if row[charIndex] == "0" else right.append(row)
+                
+            # Generator Logic
+            if searchVal == 1: 
+                input = right if len(right) >= len(left) else left
+                
+            # Scrubber Logic
+            if searchVal == 0:
+                input = left if len(left) <= len(right) else right
+            
+            rowIndex += 1
+            charIndex += 1
+            left = []
+            right = []
+            
+            if len(input) == 1 and searchVal == 0:
+                print("Generator Result: ", input)
+                finalVal.append(binToDec(input[0]))
+                rowIndex = 0
+                charIndex = 0
+                searchVal = 1
+                input = arr
+                
+            if len(input) == 1 and searchVal == 1:
+                print("Scrubber Result: ", input)
+                finalVal.append(binToDec(input[0]))
+                print(finalVal[0] * finalVal[1]) 
+            
 main = Main
-main.partOne(input)
+# main.partOne(input)
 main.partTwo(input)
